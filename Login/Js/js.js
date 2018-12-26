078d91d80699e7fd3ed31cde15e5f385bdc6005c
Rebeca Antón Ruiz <rebeca.anton.ruiz@gmail.com>
2018-12-26 19:09:26 +0100

    Add files via upload

---
+++ Login/Js/js.js
@@ -0,0 +1,60 @@
+$(document).ready(function() {
+  var longitud = false,
+    minuscula = false,
+    numero = false,
+    mayuscula = false;
+  $('input[type=password]').keyup(function() {
+    var pswd = $(this).val();
+    if (pswd.length < 8) {
+      $('#length').removeClass('valid').addClass('invalid');
+      longitud = false;
+    } else {
+      $('#length').removeClass('invalid').addClass('valid');
+      longitud = true;
+    }
+
+    //validar letra
+    if (pswd.match(/[A-z]/)) {
+      $('#letter').removeClass('invalid').addClass('valid');
+      minuscula = true;
+    } else {
+      $('#letter').removeClass('valid').addClass('invalid');
+      minuscula = false;
+    }
+
+    //validar letra mayuscula
+    if (pswd.match(/[A-Z]/)) {
+      $('#capital').removeClass('invalid').addClass('valid');
+      mayuscula = true;
+    } else {
+      $('#capital').removeClass('valid').addClass('invalid');
+      mayuscula = false;
+    }
+
+    //validar numero
+    if (pswd.match(/\d/)) {
+      $('#number').removeClass('invalid').addClass('valid');
+      numero = true;
+    } else {
+      $('#number').removeClass('valid').addClass('invalid');
+      numero = false;
+    }
+  }).focus(function() {
+    $('#pswd_info').show();
+  }).blur(function() {
+    $('#pswd_info').hide();
+  });
+
+  $("#registro").submit(function(event) {
+    alert("hola");
+    if(longitud && minuscula && numero && mayuscula){
+      alert("password correcto");
+      $("#registro").submit();
+     
+    }else{
+      alert("Password invalido.");
+      event.preventDefault();
+    }
+    
+  });
+});
