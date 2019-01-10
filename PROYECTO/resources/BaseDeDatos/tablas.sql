90df222a54aa88f1ae1cc21c73bbde1d08a36b69
Moyano07 <fjmoyano07@gmail.com>
2019-01-10 13:06:22 +0100

    Update tablas.sql

--- PROYECTO/resources/BaseDeDatos/tablas.sql
+++ PROYECTO/resources/BaseDeDatos/tablas.sql
@@ -40,9 +40,9 @@ CREATE TABLE IF NOT EXISTS Cancion (
 	 id_cancion INT(10) NOT NULL AUTO_INCREMENT ,
 	 nombre  varchar(40) NOT NULL ,
 	 fecha_alta  DATE NOT NULL,
-	 id_disco  varchar(40) NOT NULL,
-	 id_autor  varchar(40) NOT NULL ,
-	 id_estilo  varchar(40) NOT NULL,
+	 id_disco  int(10) NOT NULL,
+	 id_autor  int(10) NOT NULL ,
+	 id_estilo  int(10) NOT NULL,
 	PRIMARY KEY ( id_cancion ),
 	FOREIGN KEY ( id_disco ) REFERENCES  Disco  ( id_disco ),
 	FOREIGN KEY ( id_autor ) REFERENCES  Autor  ( id_autor ),
@@ -97,3 +97,32 @@ ALTER TABLE  Lista_reproduccion   ADD CONSTRAINT Lista_reproduccion_fk1  FOREIGN
 	 nombre varchar(40) NOT NULL,
 	PRIMARY KEY ( Id_administrador )
 );
+
+/* --- Autores --- */
+
+INSERT INTO Autor (nombre) VALUES('Amaral');
+
+INSERT INTO Autor (nombre) VALUES('Manolo Garcia');
+
+
+INSERT INTO Disco (nombre) VALUES('Pajaros en la cabeza');
+
+INSERT INTO Disco (nombre) VALUES('Arena En Los bolsillos');
+
+
+INSERT INTO Estilo (nombre) VALUES('Pop');
+
+
+/* --- Amaral --- */
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('El Universo Sobre Mi',CURRENT_DATE,1,1,1);
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('Dias de verano',CURRENT_DATE,1,1,1);
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('Revolucion',CURRENT_DATE,1,1,1);
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('mi alma perdida',CURRENT_DATE,1,1,1);
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('Marta, Sebas, Guille y Los Demas',CURRENT_DATE,1,1,1);
+
+/* --- Manolo Garcia --- */
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('Prefiero El Trapecio',CURRENT_DATE,2,2,1);
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('Carbon Y Ramas Secas',CURRENT_DATE,2,2,1);
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('Del Bosque De Tu Alegria',CURRENT_DATE,2,2,1);
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('Pajaros De Barro',CURRENT_DATE,2,2,1);
+INSERT INTO Cancion (nombre,fecha_alta,id_disco,id_autor,id_estilo) VALUES('Sobre el Oscuro Abismo En Que Te Meces',CURRENT_DATE,2,2,1);
