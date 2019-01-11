b07ebd9c811129343bbde96a2a3a5fd82e0e845b
Moyano07 <fjmoyano07@gmail.com>
2019-01-11 10:27:36 +0100

    Update principal.php

--- PROYECTO/public/PHP/principal.php
+++ PROYECTO/public/PHP/principal.php
@@ -1,17 +1,19 @@
 <?php
 
+// Arrays Base de Datos
+
 $carpetas = [
     "novedades",
     "recomendados",
     "tendencias",
-    "escuchadosRecientemente",
+    "escuchados_recientemente",
     "generos"
 ];
 
 function crearImagen($dato, $numero) {
     echo '
     <div class="col-sm-12 col-md-6 col-lg-3">
-    <a href="#" class="d-block mb-4 h-100">
+    <a href="#artista?name='.$dato.'" class="d-block mb-4 h-100">
     <img class="embed-responsive-item" control
     src="../img/imgPrincipal/'.$dato.'/'.$dato.$numero.'.png"/>
     </img>
@@ -21,7 +23,7 @@ function crearImagen($dato, $numero) {
 
 function crearFila($dato) {
     echo '
-    <h1 class="my-4 text-center text-lg-left">'.$dato.'</h1>
+    <h1 class="my-4 text-center text-lg-left">'.strtoupper(str_replace("_", " ", $dato)).'</h1>
     <div class="row text-center text-lg-left">
     ';
     for ($x = 1; $x <= 4; $x++) {
@@ -40,35 +42,49 @@ function estructuraHtml($datos) {
 
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
+
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Principal</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
     crossorigin="anonymous">
   <link href="../css/principal.css" rel="stylesheet">
+
+
+
 </head>
+
 <body>
-  <header>
+<!--  <header>
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
       <div class="container">
         <div class="collapse navbar-collapse" id="navbarResponsive"><a class="navbar-brand" href="#"><img src="../img/logo.png"
               class="logo"></a></div>
         <input type="text" class="buscador" name="" placeholder="Busca aquí...">
+
+
         <a class="nav-link" href="#"><img class="usu" src="../img/usu.png"></a>
       </div>
     </nav>
-  </header>
+  </header> -->
   <main class="container">
+
+
   <?php
     estructuraHtml($carpetas);
   ?>
+
 </div>
+
+
 </main>
-  <footer class="py-5 bg-dark">
+  <!-- <footer class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
     <div class="container">
       <p class="m-0 text-center text-white">Copyright &copy; Musick 2019</p>
     </div>
-  </footer>
+  </footer> -->
 </body>
+
 </html>
