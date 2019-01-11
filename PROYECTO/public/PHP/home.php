8cb096aae8b9d6491250d102d666a88fb9a3850c
Moyano07 <fjmoyano07@gmail.com>
2019-01-11 17:34:37 +0100

    Update home.php

--- PROYECTO/public/PHP/home.php
+++ PROYECTO/public/PHP/home.php
@@ -3,100 +3,62 @@
 // Arrays Base de Datos
 
 $carpetas = [
-    "novedades",
-    "recomendados",
-    "tendencias",
-    "escuchados_recientemente",
-    "generos"
+  "novedades",
+  "recomendados",
+  "tendencias",
+  "escuchados_recientemente",
+  "generos"
 ];
 
 function crearImagen($dato, $numero) {
-    echo '
-    <div class="col-sm-12 col-md-6 col-lg-3">
-    <a href="#artista?name='.$dato.'" class="d-block mb-4 h-100">
-    <img class="embed-responsive-item" control
-    src="../img/imgPrincipal/'.$dato.'/'.$dato.$numero.'.png"/>
-    </img>
-    </a>
-    </div>';
+  echo '
+  <div class="col-sm-12 col-md-6 col-lg-3">
+  <a href="#artista?name='.$dato.'" class="d-block mb-4 h-100">
+  <img class="embed-responsive-item" control
+  src="../img/imgPrincipal/'.$dato.'/'.$dato.$numero.'.png"/>
+  </img>
+  </a>
+  </div>';
 }
 
 function crearFila($dato) {
-    echo '
-    <h1 class="my-4 text-center text-lg-left">'.strtoupper(str_replace("_", " ", $dato)).'</h1>
-    <div class="row text-center text-lg-left">
-    ';
-    for ($x = 1; $x <= 4; $x++) {
-        crearImagen($dato, $x);
-    }
-    echo '</div>';
+  echo '
+  <h1 class="my-4 text-center text-lg-left">'.strtoupper(str_replace("_", " ", $dato)).'</h1>
+  <div class="row text-center text-lg-left">
+  ';
+  for ($x = 1; $x <= 4; $x++) {
+    crearImagen($dato, $x);
+  }
+  echo '</div>';
 }
 
 function estructuraHtml($datos) {
-    foreach ($datos as $nombre) {
-        crearFila($nombre);
-    }
+  foreach ($datos as $nombre) {
+    crearFila($nombre);
+  }
 }
 
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-
 <head>
-
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <title>Principal</title>
   <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS"
-    crossorigin="anonymous">
+  crossorigin="anonymous">
   <link href="../css/home.css" rel="stylesheet">
-
-
-<<<<<<< HEAD:PROYECTO/public/PHP/home.php
-=======
-
->>>>>>> 1719bd0206d04fe6a1be2c3138ed3011d95036f3:proyecto_0.1/php/principal.php
-
 </head>
-
 <body>
-<!--  <header>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-      <div class="container">
-        <div class="collapse navbar-collapse" id="navbarResponsive"><a class="navbar-brand" href="#"><img src="../img/logo.png"
-              class="logo"></a></div>
-        <input type="text" class="buscador" name="" placeholder="Busca aquí...">
-
-
-        <a class="nav-link" href="#"><img class="usu" src="../img/usu.png"></a>
-      </div>
-    </nav>
-<<<<<<< HEAD:PROYECTO/public/PHP/home.php
-  </header>   -->
-=======
-  </header> -->
->>>>>>> 1719bd0206d04fe6a1be2c3138ed3011d95036f3:proyecto_0.1/php/principal.php
   <main class="container">
-
-
-  <?php
+    <?php
     estructuraHtml($carpetas);
-  ?>
-
-</div>
+    ?>
+  </div>
 
 
 </main>
-<<<<<<< HEAD:PROYECTO/public/PHP/home.php
-  <!--<footer class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
-=======
-  <!-- <footer class="navbar navbar-expand-lg navbar-dark bg-dark fixed-bottom">
->>>>>>> 1719bd0206d04fe6a1be2c3138ed3011d95036f3:proyecto_0.1/php/principal.php
-    <div class="container">
-      <p class="m-0 text-center text-white">Copyright &copy; Musick 2019</p>
-    </div>
-  </footer> -->
 </body>
 
 </html>
