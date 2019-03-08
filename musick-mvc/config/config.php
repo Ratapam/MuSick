6a051021bea14e0ae98118a7f408d572ea54facc
admon <ejemplo@gmail.com>
2019-03-08 18:43:02 +0100

    Actualizacion inicio

--- musick-mvc/config/config.php
+++ musick-mvc/config/config.php
@@ -7,7 +7,8 @@ Config::set('db.name', 'musick');
 // Config::set('db.user', 'administrador');
 // Config::set('db.pass', '1234');
 Config::set('db.user', 'admon');
-Config::set('db.pass', 'admon_1234');
+//Config::set('db.pass', 'admon_1234');
+Config::set('db.pass', '1234');
 
 Config::set('ruta.defecto','/usuario/principal/');
 
